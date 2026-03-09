04cbbf02173df4002f3ed77fcb08c9d43d17eb2f
Lakshmipathi J <109786244+Lakshmipathi17@users.noreply.github.com>
2026-03-09 17:06:30 +0530

    vite build issues

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
 // <reference types="vite/client" />
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.png";
+declare module "*.svg";
+declare module "*.webp";
+declare module "*.mp4";
+declare module "*.pdf";
+declare module "*.docx";
+declare module "*.xlsx";
+declare module "*.json";
+declare module "*.txt";
+declare module "*.avif";
